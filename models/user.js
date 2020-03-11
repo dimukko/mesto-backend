5fb1fc2bae3aa034ab8f5155b61dbe1f512c3ad3
dimukko <dimukko@gmail.com>
2020-03-11 10:54:41 +0700

    Add models&schemas

---
+++ models/user.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-unresolved */
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return validator.isURL(link);
+      },
+    },
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
