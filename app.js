9e20003b81c8d8c0ea0e263d40c2d6684ba15e22
dimukko <dimukko@gmail.com>
2020-03-11 13:33:09 +0700

    Succesfull user creation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
@@ -9,6 +10,9 @@ const error = require('./routes/error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const users = require('../data/user.json');
+const { getUsers, getUserById, createUser } = require('../controllers/users');
 
-router.get('/', (req, res) => res.json(users));
-router.get('/:id', (req, res) => {
-  const userId = req.params.id;
-  // eslint-disable-next-line no-underscore-dangle
-  const userIsFind = users.find((user) => user._id === userId);
-  if (userIsFind) return res.json(userIsFind);
-  return res.status(404).json({ message: 'Нет пользователя с таким id' });
-});
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+
+router.post('/', createUser);
+
 module.exports = router;
