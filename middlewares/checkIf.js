6ab4355cec7e6af358e4c80ff45e98e5ffcf8151
dimukko <dimukko@gmail.com>
2020-04-01 10:44:33 +0700

    Исправил роуты в соответствии с заданием

--- middlewares/checkIf.js
+++ middlewares/checkIf.js
@@ -12,29 +12,14 @@ const checkCardObjectId = (req, res, next) => {
 
 // проверка объекта пользователя в запросе
 const checkUserObjectId = (req, res, next) => {
-  if (!ObjectId.isValid(req.user._id) || !ObjectId.isValid(req.params.id)) {
+  if (!ObjectId.isValid(req.user._id)) {
     res.status(400).send({ message: messages.user.id.isNotValid });
   } else {
     next();
   }
 };
 
-// проверка объекта пользователя и владельца в запросе
-const checkUserObjectIdAndParamId = (req, res, next) => {
-  const { _id: userId } = req.user;
-  const { id: userParamId } = req.params;
-
-  if (ObjectId.isValid(userId) && userId === userParamId) {
-    next();
-  } else if (!ObjectId.isValid(userParamId)) {
-    res.status(400).send({ message: messages.user.id.isNotValid });
-  } else if (userId !== userParamId) {
-    res.status(401).send({ message: messages.authorization.isRequired });
-  }
-};
-
 module.exports = {
   checkCardObjectId,
   checkUserObjectId,
-  checkUserObjectIdAndParamId,
 };
