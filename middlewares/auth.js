11fd03df73f2d55c94e3aef7b7c504264e8613d5
dimukko <dimukko@gmail.com>
2020-04-05 03:24:37 +0700

    Ночью кодить - ловить нелепые ошибки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 const { messages } = require('../tools/messages');
 const settings = require('../appconfig');
 const User = require('../models/user');
-const { UnauthorizedErr } = require('../errors/index');
+const { UnauthorizedErr: NotAuthorizedErr } = require('../errors/index');
 
 // авторизация и запись пэйлоуда в запрос
 const auth = (req, res, next) => {
   const { jwt: token } = req.cookies;
 
   if (!jwt) {
-    return next(new UnauthorizedErr(messages.authorization.isRequired));
+    return next(new NotAuthorizedErr(messages.authorization.isRequired));
   }
 
   let payload;
@@ -17,7 +17,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, settings.JWT_SECRET);
   } catch (err) {
-    return next(new UnauthorizedErr(messages.authorization.isRequired));
+    return next(new NotAuthorizedErr(messages.authorization.isRequired));
   }
 
   req.user = payload;
@@ -25,7 +25,7 @@ const auth = (req, res, next) => {
   return User.findById(req.user._id)
     .orFail()
     .then(() => next())
-    .catch((err) => next(new UnauthorizedErr({ error: err.message })));
+    .catch((err) => next(new NotAuthorizedErr({ error: err.message })));
 };
 
 module.exports = { auth };
